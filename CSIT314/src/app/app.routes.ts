36b7060b5f9c8271efc7e92a30de54318fcceeb2
Emmanuelgeor <emmanualgeorge99@gmail.com>
2024-11-12 19:18:57 +0800

    Update app.routes.ts

--- CSIT314/src/app/app.routes.ts
+++ CSIT314/src/app/app.routes.ts
@@ -1,11 +1,19 @@
 import { Routes } from '@angular/router';
 
-import { LoginComponent } from './views/login/login.component'; // Make sure to import your LoginComponent
-//import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './views/login/login.component';
+import { CreateProfileComponent } from './views/create-profile/create-profile.component';
+import { ViewProfileComponent } from './views/view-profile/view-profile.component';
+import { UpdateProfileComponent } from './views/update-profile/update-profile.component';
+import { SuspendProfileComponent } from './views/suspend-profile/suspend-profile.component';
+import { SearchProfileComponent } from './views/search-profile/search-profile.component';
+import { LogoutComponent } from './views/logout/logout.component';
 
-
-//add new routes here
 export const routes: Routes = [
-    //{ path: '', component: HomeComponent },       // Default route
-    { path: 'login', component: LoginComponent }, // Login route
+    { path: 'login', component: LoginComponent },                 // Login route
+    { path: 'create-profile', component: CreateProfileComponent }, // Create Profile route
+    { path: 'view-profile/:id', component: ViewProfileComponent }, // View Profile route (dynamic ID)
+    { path: 'update-profile/:id', component: UpdateProfileComponent }, // Update Profile route (dynamic ID)
+    { path: 'suspend-profile', component: SuspendProfileComponent },   // Suspend Profile route
+    { path: 'search-profile', component: SearchProfileComponent },     // Search Profile route
+    { path: 'logout', component: LogoutComponent },                    // Logout route
 ];
